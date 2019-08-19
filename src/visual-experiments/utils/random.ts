267ccdbb86db5e78409600257bd6cf3954d1d222
Eduardo San Martin Morote <posva13@gmail.com>
2019-08-19 15:08:03 +0200

    add randomizer

---
+++ src/visual-experiments/utils/random.ts
@@ -0,0 +1,65 @@
+// A port of an algorithm by Johannes Baagøe <baagoe@baagoe.com>, 2010
+// http://baagoe.com/en/RandomMusings/javascript/
+// https://github.com/nquinlan/better-random-numbers-for-javascript-mirror
+// Original work is under MIT license -
+
+function createMasher() {
+  let n = 0xefc8249d
+
+  return (data: string) => {
+    for (let i = 0; i < data.length; i++) {
+      n += data.charCodeAt(i)
+      let h = 0.02519603282416938 * n
+      n = h >>> 0
+      h -= n
+      h *= n
+      n = h >>> 0
+      h -= n
+      n += h * 0x100000000 // 2^32
+    }
+    return (n >>> 0) * 2.3283064365386963e-10 // 2^-32
+  }
+}
+
+export class Randomizer {
+  private c: number
+  private s0: number
+  private s1: number
+  private s2: number
+
+  constructor(seed: string) {
+    const mash = createMasher()
+    // Apply the seeding algorithm from Baagoe.
+    this.c = 1
+    this.s0 = mash(' ')
+    this.s1 = mash(' ')
+    this.s2 = mash(' ')
+    this.s0 -= mash(seed)
+    if (this.s0 < 0) {
+      this.s0 += 1
+    }
+    this.s1 -= mash(seed)
+    if (this.s1 < 0) {
+      this.s1 += 1
+    }
+    this.s2 -= mash(seed)
+    if (this.s2 < 0) {
+      this.s2 += 1
+    }
+  }
+
+  next() {
+    let t = 2091639 * this.s0 + this.c * 2.3283064365386963e-10 // 2^-32
+    this.s0 = this.s1
+    this.s1 = this.s2
+    return (this.s2 = t - (this.c = t | 0))
+  }
+
+  int32() {
+    return (this.next() * 0x100000000) | 0
+  }
+
+  double() {
+    return this.next() + ((this.next() * 0x200000) | 0) * 1.1102230246251565e-16 // 2^-53
+  }
+}
