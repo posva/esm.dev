890bd48f464467be208200fe50b51380c3e1218d
Eduardo San Martin Morote <posva13@gmail.com>
2019-08-26 18:27:18 +0200

    more

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ const config: Configuration = {
   mode: 'universal',
 
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'Eduardo San Martin Morote',
 
     htmlAttrs: {
       lang: 'en',
@@ -18,8 +18,29 @@ const config: Configuration = {
         name: 'description',
         content: `Eduardo's personal website`,
       },
+      // <meta name="theme-color" content="black" />
+      // <meta name="msapplication-navbutton-color" content="black" />
+      { name: 'apple-mobile-web-app-capable', content: 'yes' },
+      // <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+
+      // Open Graph data
+      { property: 'og:title', content: 'Eduardo San Martin Morote' },
+      { property: 'og:type', content: 'website' },
+      { property: 'og:url', content: 'https://esm.dev' },
+      { property: 'og:image', content: 'https://esm.dev/media-preview.jpg' },
+      { property: 'og:description', content: 'Hello! I am Eduardo,' },
+      // Twitter Card data
+      { name: 'twitter:card', content: 'summary' },
+      { name: 'twitter:site', content: '@posva' },
+      { name: 'twitter:creator', content: '@posva' },
+      { name: 'twitter:title', content: 'Eduardo San Martin Morote' },
+      { name: 'twitter:description', content: 'Hello! I am Eduardo.' },
+      { name: 'twitter:image', content: 'https://esm.dev/media-preview.jpg' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/png', href: '/favicon.png' },
+    ],
   },
 
   loading: { color: '#fff' },
