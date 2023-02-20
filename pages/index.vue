f61f2ddd929e4b96b723cecd15dcdefa838f6df7
Eduardo San Martin Morote <posva13@gmail.com>
2023-02-20 16:53:25 +0100

    perf remove old stuff

--- pages/index.vue
+++ pages/index.vue
@@ -1,70 +1,7 @@
 <script setup lang="ts">
-import { rotateOffsets } from '~/lab/dom/links'
 import { vMagnetic } from '@/directives/vMagnetic'
 
-let lastElapsed = 0
-const BASE_DELTA = 1000 / 60 // 1s / 60 frames
-let lastDelta = BASE_DELTA
 
-function stepper(elapsed: number) {
-  lastDelta = elapsed - lastElapsed
-  lastElapsed = elapsed
-  const ratio = lastDelta / BASE_DELTA
-
-  if (Date.now() - lastMove > 200) {
-    mouseTravel = Math.max(0, mouseTravel - ratio * 70)
-  }
-
-  const route = useRoute()
-  // only on production because we add css variables on root and it's hard to debug
-  if (process.env.NODE_ENV === 'production' || route.query.links === 'no') {
-    rotateOffsets(ratio, Math.max(1, Math.min(mouseTravel / 200, 10)))
-  }
-}
-
-const lastMousePos = [-1, -1]
-let mouseTravel = 0
-let lastMove = 0
-function mouseMoveListener({ pageX, pageY }: MouseEvent) {
-  if (lastMousePos[0] > -1)
-    mouseTravel += Math.max(
-      Math.abs(pageX - lastMousePos[0]),
-      Math.abs(pageY - lastMousePos[1])
-    )
-  lastMousePos[0] = pageX
-  lastMousePos[1] = pageY
-  lastMove = Date.now()
-}
-
-let rafId: number
-function update() {
-  rafId = requestAnimationFrame((elapsed) => {
-    stepper(elapsed)
-    update()
-  })
-}
-
-onMounted(() => {
-  // For better dev
-  if (import.meta.hot) {
-    import.meta.hot.accept('~/lab/dom/links', () => {
-      cancelAnimationFrame(rafId)
-      update()
-    })
-  }
-
-  // document.querySelectorAll('a').forEach((link) => {
-  //   link.dataset['text'] = link.innerText
-  // })
-
-  update()
-
-  document.body.addEventListener('mousemove', mouseMoveListener)
-})
-
-onUnmounted(() => {
-  document.body.removeEventListener('mousemove', mouseMoveListener)
-})
 </script>
 
 <template>
@@ -83,28 +20,19 @@ onUnmounted(() => {
           a Frontend Nerd. I work as a consultant to help you keep your
           applications maintainable. I also give
           <a v-magnetic href="#TODO">Vue.js trainings</a> and
-          <a
-            href="https://www.youtube.com/results?search_query=eduardo+san+martin+morote"
-            v-magnetic
-            >talk at conferences</a
-          >. I write some
+          <a href="https://www.youtube.com/results?search_query=eduardo+san+martin+morote" v-magnetic>talk at
+            conferences</a>. I write some
           <a v-magnetic href="https://github.com/posva">useful libraries</a>,
           mostly for Vue, but not only, and post them on Github. I may not
           <i>
-            <a
-              v-magnetic
-              href="https://twitter.com/posva/status/1146415898967908352"
-              >take myself very seriously</a
-            >
+            <a v-magnetic href="https://twitter.com/posva/status/1146415898967908352">take myself very seriously</a>
           </i>
           but take my job very seriously and loves solving problems instead of
           complaining about them.
         </p>
         <p class="my-4">
           If you want to work together, please
-          <a v-magnetic href="https://twitter.com/posva"
-            >send me a PM on Twitter</a
-          >.
+          <a v-magnetic href="https://twitter.com/posva">send me a PM on Twitter</a>.
         </p>
       </main>
     </div>
