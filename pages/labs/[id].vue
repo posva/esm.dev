ee7f1c66511d20b32b719afb505ddd4de9f231fe
Eduardo San Martin Morote <posva13@gmail.com>
2023-02-20 16:25:29 +0100

    comment lab

--- pages/labs/[id].vue
+++ pages/labs/[id].vue
@@ -8,20 +8,20 @@ const labId = computed(() => {
 
 <template>
   <div id="lab" class="min-h-full">
-    <LabExperiment :lab-id="labId" />
+  <LabExperiment :lab-id="labId" />
 
-    <div class="my-4"></div>
+  <!-- <div class="my-4"></div> -->
 
-    <section class="px-4">
+  <!-- <section class="px-4">
       <h1>Hey there</h1>
 
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque,
-        deserunt minima! Laudantium, fuga cumque. Assumenda modi eveniet nam
-        facilis error, et ab rem magnam nemo blanditiis? Odio earum repellendus
-        quisquam!
-      </p>
-    </section>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque,
+          deserunt minima! Laudantium, fuga cumque. Assumenda modi eveniet nam
+          facilis error, et ab rem magnam nemo blanditiis? Odio earum repellendus
+          quisquam!
+        </p>
+      </section> -->
   </div>
 </template>
 
