3c2c93425cd77011b87662cd6b8c46014f8cf4a7
Eduardo San Martin Morote <posva13@gmail.com>
2023-11-15 15:36:47 +0100

    updates

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
 
   css: ['@/assets/css/main.css'],
 
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/content', 'nuxt-icon'],
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/content', 'nuxt-icon', '@vueuse/nuxt'],
 
   experimental: {
     typedPages: true,
