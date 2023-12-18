f80bbbfdf377742fee45fe86413929fe5267be26
Eduardo San Martin Morote <posva13@gmail.com>
2023-12-18 15:25:08 +0100

    workshop page

---
+++ pages/workshops/index.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup></script>
+
+<template>
+  <main
+    class="w-full max-w-xl pt-16 mx-auto prose lg:prose-xl dark:prose-invert prose-stone"
+  >
+    <ContentDoc></ContentDoc>
+  </main>
+</template>
